d362157987c16dca693705b9db30d2f3386c0b54
Felix Kling <sprinterl@gmx.de>
2011-08-30 23:56:26 +0200

    - Keys are changed when composition changes - New parameter, 'useKeyFrom' can be used to specify an other action, the cacheKey should be used form

--- mide/processor/ServiceCall.js
+++ mide/processor/ServiceCall.js
@@ -1,17 +1,25 @@
 goog.provide('org.reseval.processor.ServiceCall');
 goog.require('mide.core.net');
 goog.require('mide.core.Session');
+goog.require('mide.core.Composition');
 
 goog.require('goog.dom');
+goog.require('goog.events');
 
 org.reseval.processor.ServiceCall = function(config) {
 	this.config = config || {};
 	this.data = {};
 	this.output = goog.dom.createElement('div');
+	
+	this.n = 0;
 };
 
 org.reseval.processor.ServiceCall.prototype.setComponentInstance = function(component) {
 	this.component = component;
+	
+	goog.events.listen(this.component.getComposition(), mide.core.Composition.Events.CHANGE, function() {
+		this.n++;
+	}, null, this);
 };
 
 org.reseval.processor.ServiceCall.prototype.perform = function(operation, params, next) {
@@ -24,13 +32,14 @@ org.reseval.processor.ServiceCall.prototype.perform = function(operation, params
 		this.data[operation].cacheKey = controlData.cacheKey;
 	}
 	else {
-		this.data[operation].cacheKey = 1234;// mide.core.Session.getInstance().getId() + this.component.getId();
+		this.data[operation].cacheKey = mide.core.Session.getInstance().getId() + this.component.getId() + this.n;
 	}
 	
 	if(config && config.url) {
 		mide.core.net.makeRequest({
 			url: config.url, 
-			parameters: {key: this.data[operation].cacheKey, data: 'yes', fetch: 10}, 
+			parameters: {key: this.data[operation].cacheKey, data: 'yes', fetch: 10},
+			responseFormat: 'json',
 			complete: function(response) {
 				self.output.innerHTML = JSON.stringify(response);
 				self.data[operation].cacheKey = response.cacheKey;
@@ -59,16 +68,21 @@ org.reseval.processor.ServiceCall.prototype.triggerEvent = function(event, param
 
 org.reseval.processor.ServiceCall.prototype.makeRequest = function(name, url, getParams, postParams, next) {
 	var config = this.config[name];
-	
+	getParams = getParams || {};
 	
 	if(config && config.url) {
 		this.data[name] = {};
-		this.data[name].cacheKey =  1234; //mide.core.Session.getInstance().getId() + this.component.getId();
+		if(config.useKeyFrom) {
+			this.data[name].cacheKey = this.data[config.useKeyFrom].cacheKey;
+		}
+		else {
+			this.data[name].cacheKey =  mide.core.Session.getInstance().getId() + this.component.getId() + this.n;
+		}
 		
 		url = config.url;
 		getParams.key = this.data[name].cacheKey;
 		getParams.data = 'yes';
-		getParams.fetch = 10;		
+		getParams.fetch = 10;
 	}
 	next(name, url, getParams, postParams);
 };
@@ -76,7 +90,9 @@ org.reseval.processor.ServiceCall.prototype.makeRequest = function(name, url, ge
 org.reseval.processor.ServiceCall.prototype.makeResponse = function(name, response) {
 		this.output.innerHTML = response;
 		response = JSON.parse(response);
-		this.data[name].cacheKey = response.cacheKey;
+		if(response.cacheKey) {
+			this.data[name].cacheKey = response.cacheKey;
+		}
 		this.data[name].dataObject = response.dataObject;
 		
 		return JSON.stringify(response.dataObject);
