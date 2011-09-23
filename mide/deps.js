742b480d7b00ca2982b8c114165e9e567f70dc6f
Felix Kling <sprinterl@gmx.de>
2011-09-23 23:34:37 +0200

    Updated dependencies

--- mide/deps.js
+++ mide/deps.js
@@ -8,15 +8,16 @@ goog.addDependency('../../../mide/core/Session.js', ['mide.config.session', 'mid
 goog.addDependency('../../../mide/core/component/Event.js', ['mide.core.Event'], []);
 goog.addDependency('../../../mide/core/component/Operation.js', ['mide.core.Operation'], []);
 goog.addDependency('../../../mide/core/component/Parameter.js', ['mide.core.Parameter'], []);
-goog.addDependency('../../../mide/core/component/Request.js', ['mide.core.Request'], []);
+goog.addDependency('../../../mide/core/composition/ArgumentMapper.js', ['mide.core.ArgumentMapper'], []);
 goog.addDependency('../../../mide/core/composition/Connection.js', ['mide.core.composition.Connection'], []);
+goog.addDependency('../../../mide/core/composition/DataTypeMapper.js', ['mide.core.DataTypeMapper'], ['goog.array', 'goog.object']);
 goog.addDependency('../../../mide/core/net.js', ['mide.config.net', 'mide.core.net'], ['goog.Uri', 'goog.events', 'goog.net.XhrIo', 'goog.net.XhrIoPool', 'goog.object', 'goog.uri.utils']);
 goog.addDependency('../../../mide/core/parser.js', ['mide.parser'], ['goog.dom.xml']);
 goog.addDependency('../../../mide/mapper/ComponentMapper.js', ['mide.mapper.ComponentMapper'], ['mide.core.Event', 'mide.core.Operation', 'mide.core.Parameter']);
 goog.addDependency('../../../mide/mapper/CompositionMapper.js', ['mide.mapper.CompositionMapper'], ['mide.core.Composition', 'mide.core.composition.Connection']);
 goog.addDependency('../../../mide/mapper/EMDLMapper.js', ['mide.mapper.EMDLMapper'], ['goog.array', 'goog.dom.xml', 'mide.mapper.ComponentMapper', 'mide.parser']);
 goog.addDependency('../../../mide/mapper/JSONMapper.js', ['mide.mapper.JSONMapper'], ['goog.json', 'mide.mapper.CompositionMapper']);
-goog.addDependency('../../../mide/mashupIDE.js', ['MashupIDE'], ['goog.array', 'goog.object', 'goog.ui.Component', 'mide.core.Session', 'mide.core.registry.ServerRegistry']);
+goog.addDependency('../../../mide/mashupIDE.js', ['MashupIDE'], ['goog.array', 'goog.object', 'goog.ui.Component', 'mide.core.Composition', 'mide.core.Session', 'mide.core.registry.ServerRegistry']);
 goog.addDependency('../../../mide/module/moduleloader.js', ['mide.module.ModuleLoader'], ['goog.module.ModuleLoader']);
 goog.addDependency('../../../mide/module/modulemanager.js', ['mide.module.ModuleManager'], ['goog.module.ModuleManager', 'mide.module.ModuleLoader']);
 goog.addDependency('../../../mide/processor/DataProcessor.js', ['mide.processor.DataProcessor'], ['goog.array']);
@@ -24,7 +25,7 @@ goog.addDependency('../../../mide/processor/DomainConceptMapper.js', ['org.resev
 goog.addDependency('../../../mide/processor/JsonProcessorProvider.js', ['mide.processor.JsonProcessorProvider'], ['mide.processor.ProcessorProvider']);
 goog.addDependency('../../../mide/processor/ProcessorManager.js', ['mide.processor.ProcessorManager'], ['goog.array']);
 goog.addDependency('../../../mide/processor/ProcessorProvider.js', ['mide.processor.ProcessorProvider'], ['mide.processor.ProcessorManager']);
-goog.addDependency('../../../mide/processor/ServiceCall.js', ['org.reseval.processor.ServiceCall'], ['goog.array', 'goog.dom', 'goog.events', 'mide.core.Component', 'mide.core.Composition', 'mide.core.Session', 'mide.core.net', 'mide.processor.DataProcessor']);
+goog.addDependency('../../../mide/processor/ServiceCall.js', ['org.reseval.processor.ServiceCall'], ['goog.array', 'goog.dom', 'goog.events', 'mide.core.Component', 'mide.core.Composition', 'mide.core.Session', 'mide.core.net', 'mide.processor.DataProcessor', 'mide.util.OptionMap']);
 goog.addDependency('../../../mide/registry/base_registry.js', ['mide.config.registry', 'mide.core.registry', 'mide.core.registry.BaseRegistry'], []);
 goog.addDependency('../../../mide/registry/localstorage_registry.js', ['mide.registry.LocalstorageRegistry'], ['goog.array', 'goog.storage.mechanism.HTML5LocalStorage', 'mide.Component', 'mide.registry.BaseRegistry']);
 goog.addDependency('../../../mide/registry/serverregistry.js', ['mide.core.registry.ServerRegistry'], ['goog.array', 'goog.json', 'goog.object', 'goog.uri.utils', 'mide.core.ComponentDescriptor', 'mide.core.net', 'mide.core.registry.BaseRegistry']);
